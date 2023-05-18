14135a53cdfdbc97a78bbf181e2a40ecb5491b4f
SergiMartin06 <smartin19238@iesjoanramis.org>
2023-05-18 12:40:25 +0200

    Cambio local

--- Projecto_Python/Projecte_Python.py
+++ Projecto_Python/Projecte_Python.py
@@ -1,6 +1,6 @@
 import random
 import os
-
+import tkinter
 
 def menu():
     print("""
@@ -15,6 +15,7 @@ def menu():
     
     a = int(input("Selecciona una opcion: "))
     return a
+
 
 def Llistes_NombresAleatoris():
     l = ["Mapa", "Catalejo", "Chancla", "Guante", "Cuchillo"]
@@ -31,25 +32,168 @@ def Llistes_NombresAleatoris():
     elif objeto == "Cuchillo":
         print("Sobrevives")
 
+
+def menu_agenda():
+    print("""
+    
+    1. Añadir elementos en la lista
+    
+    2. Ver la lista
+    
+    
+    """)
+
+    a=input("Escoge una opcion: ")
+    return a
+
+
+
 def agenda():
     segunda_funcion="Agenda"
     a=0
     while a!=".":
         if a!=".":
-            with open("/home/cicles/archivo.txt", "a") as f:
+            with open("/home/Cicles/archivo.txt", "a") as f:
                 a=input("Añade tus planes: ")
                 f.write(a+"\n")
                 f.read()
+          
 
 
 
+def ahorcado():
+    AHORCADO = ['''
+        +---+
+        |   |
+            |
+            |
+            |
+            |
+        =========''', '''
+        +---+
+        |   |
+        O   |
+            |
+            |
+            |
+        =========''', '''
+        +---+
+        |   |
+        O   |
+        |   |
+            |
+            |
+        =========''', '''
+        +---+
+        |   |
+        O   |
+        /|   |
+            |
+            |
+        =========''', '''
+        +---+
+        |   |
+        O   |
+        /|\  |
+            |
+            |
+        =========''', '''
+        +---+
+        |   |
+        O   |
+        /|\  |
+        /    |
+            |
+        =========''', '''
+        +---+
+        |   |
+        O   |
+        /|\  |
+        / \  |
+            |
+        =========''']
+    palabras = 'valoracion aprenderpython comida juego python web imposible variable curso volador cabeza reproductor mirada escritor billete lapicero celular valor revista gratuito disco voleibol anillo estrella'.split()
+ 
+def buscarPalabraAleat(listaPalabras):
+    # Esta funcion retorna una palabra aleatoria.
+    palabraAleatoria = random.randint(0, len(listaPalabras) - 1)
+    return listaPalabras[palabraAleatoria]
+ 
+def displayBoard(AHORCADO, letraIncorrecta, letraCorrecta, palabraSecreta):
+    print(AHORCADO[len(letraIncorrecta)])
+    print ("")
+    fin = " "
+    print ('Letras incorrectas:', fin)
+    for letra in letraIncorrecta:
+        print (letra, fin)
+    print ("")
+    espacio = '_' * len(palabraSecreta)
+    for i in range(len(palabraSecreta)): # Remplaza los espacios en blanco por la letra bien escrita
+        if palabraSecreta[i] in letraCorrecta:
+            espacio = espacio[:i] + palabraSecreta[i] + espacio[i+1:]
+    for letra in espacio: # Mostrará la palabra secreta con espacios entre letras
+        print (letra, fin)
+    print ("")
+ 
+def elijeLetra(algunaLetra):
+    # Devuelve la letra que el jugador ingreso. Esta función hace que el jugador ingrese una letra y no cualquier otra cosa
+    while True:
+        print ('Adivina una letra:')
+        letra = input()
+        letra = letra.lower()
+        if len(letra) != 1:
+            print ('Introduce una sola letra.') 
+        elif letra in algunaLetra:
+            print ('Ya has elegido esa letra ¿Qué tal si pruebas con otra?')
+        elif letra not in 'abcdefghijklmnopqrstuvwxyz':
+            print ('Elije una letra.')
+        else:
+            return letra
+ 
+def empezar():
+    # Esta funcion devuelve True si el jugador quiere volver a jugar, de lo contrario devuelve False
+    print ('Quieres jugar de nuevo? (Si o No)')
+    return input().lower().startswith('s')
 
 
+def juego_a():
+    print ('A H O R C A D O')
+    letraIncorrecta = ""
+    letraCorrecta = ""
+    palabraSecreta = buscarPalabraAleat(palabras)
+    finJuego = False
+    while True:
+        displayBoard(AHORCADO, letraIncorrecta, letraCorrecta, palabraSecreta)
+        # El usuairo elije una letra.
+        letra = elijeLetra(letraIncorrecta + letraCorrecta)
+        if letra in palabraSecreta:
+            letraCorrecta = letraCorrecta + letra
+            # Se fija si el jugador ganó
+            letrasEncontradas = True
+            for i in range(len(palabraSecreta)):
+                if palabraSecreta[i] not in letraCorrecta:
+                    letrasEncontradas = False
+                    break
+            if letrasEncontradas:
+                print ('¡Muy bien! La palabra secreta es "' + palabraSecreta + '"! ¡Has ganado!')
+                finJuego = True
+        else:
+            letraIncorrecta = letraIncorrecta + letra
+            # Comprueba la cantidad de letras que ha ingresado el jugador y si perdió
+            if len(letraIncorrecta) == len(AHORCADO) - 1:
+                displayBoard(AHORCADO, letraIncorrecta, letraCorrecta, palabraSecreta)
+                print ('¡Se ha quedado sin letras!\nDespues de ' + str(len(letraIncorrecta)) + ' letras erroneas y ' + str(len(letraCorrecta)) + ' letras correctas, la palabra era "' + palabraSecreta + '"')
+                finJuego = True
+        # Pregunta al jugador si quiere jugar de nuevo
+        if finJuego:
+            if empezar():
+                letraIncorrecta = ""
+                letraCorrecta = ""
+                finJuego = False
+                palabraSecreta = buscarPalabraAleat(palabras)
+            else:
+                break
 
-
-
-
-                
 
 
 
@@ -67,6 +211,10 @@ while a != 0:
             print ("Has escogido la 2 opcion")
             segunda_funcion=agenda()
         
+        case 3:
+            print("Has escogido la opcion 3")
+            juego_a
+
         case other:
             print("Opcion no valida")
 
--- Projecto_Python/ahorcado.py
+++ Projecto_Python/ahorcado.py
@@ -1,139 +1,134 @@
-import random as random
+import random
 
-def armarBaraja():
-    baraja = []
-    #EJEMPLO CARTA: ROJA 6, VERDE 4, AMARILLO SALTADO
-    colores = ["ROJO","VERDE","AZUL","AMARILLO"]
-    valores = [0,1,2,3,4,5,6,7,8,9,"+2", "SALTADO", "REVERSA"]
-    comodines = ["COMODIN","COMODIN +4"]
-    for color in colores:
-        for valor in valores:
-            cartaVal = "{} {}".format(color, valor)
-            baraja.append(cartaVal)
-            if valor != 0:
-                baraja.append(cartaVal)
-                for i in range(4):
-                    baraja.append(comodines[0])
-                    baraja.append(comodines[1])
-            return baraja
+AHORCADO = ['''
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     /    |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     / \  |
+          |
+    =========''']
+palabras = 'sergi''epi''kebab''comida''hamburgesa'.split()
+ 
+def buscarPalabraAleat(listaPalabras):
+    # Esta funcion retorna una palabra aleatoria.
+    palabraAleatoria = random.randint(0, len(listaPalabras) - 1)
+    return listaPalabras[palabraAleatoria]
+ 
+def displayBoard(AHORCADO, letraIncorrecta, letraCorrecta, palabraSecreta):
+    print(AHORCADO[len(letraIncorrecta)])
+    print ("")
+    fin = " "
+    print ('Letras incorrectas:', fin)
+    for letra in letraIncorrecta:
+        print (letra, fin)
+    print ("")
+    espacio = '_' * len(palabraSecreta)
+    for i in range(len(palabraSecreta)): # Remplaza los espacios en blanco por la letra bien escrita
+        if palabraSecreta[i] in letraCorrecta:
+            espacio = espacio[:i] + palabraSecreta[i] + espacio[i+1:]
+    for letra in espacio: # Mostrará la palabra secreta con espacios entre letras
+        print (letra, fin)
+    print ("")
+ 
+def elijeLetra(algunaLetra):
+    # Devuelve la letra que el jugador ingreso. Esta función hace que el jugador ingrese una letra y no cualquier otra cosa
+    while True:
+        print ('Adivina una letra:')
+        letra = input()
+        letra = letra.lower()
+        if len(letra) != 1:
+            print ('Introduce una sola letra.') 
+        elif letra in algunaLetra:
+            print ('Ya has elegido esa letra ¿Qué tal si pruebas con otra?')
+        elif letra not in 'abcdefghijklmnopqrstuvwxyz':
+            print ('Elije una letra.')
+        else:
+            return letra
+ 
+def empezar():
+    # Esta funcion devuelve True si el jugador quiere volver a jugar, de lo contrario devuelve False
+    print ('Quieres jugar de nuevo? (Si o No)')
+    return input().lower().startswith('s')
 
-def shuffleDeck(baraja):
-    for cartaPos in range(len(baraja)):
-        randPos=random.randint(0, 107);
-        baraja[cartaPos], baraja[randPos] = baraja[randPos], baraja[cartaPos]
-    return baraja
 
 
-def sacarCarta(numCartas):
-    cartasDib = []
-    for x in range(numCartas):
-        cartasDib.append(unoBaraja.pop(0))
-    return cartasDib
+print ('A H O R C A D O')
+letraIncorrecta = ""
+letraCorrecta = ""
+palabraSecreta = buscarPalabraAleat(palabras)
+finJuego = False
+while True:
+    displayBoard(AHORCADO, letraIncorrecta, letraCorrecta, palabraSecreta)
+    # El usuairo elije una letra.
+    letra = elijeLetra(letraIncorrecta + letraCorrecta)
+    if letra in palabraSecreta:
+        letraCorrecta = letraCorrecta + letra
+        # Se fija si el jugador ganó
+        letrasEncontradas = True
+        for i in range(len(palabraSecreta)):
+            if palabraSecreta[i] not in letraCorrecta:
+                letrasEncontradas = False
+                break
+        if letrasEncontradas:
+            print ('¡Muy bien! La palabra secreta es "' + palabraSecreta + '"! ¡Has ganado!')
+            finJuego = True
+    else:
+        letraIncorrecta = letraIncorrecta + letra
+        # Comprueba la cantidad de letras que ha ingresado el jugador y si perdió
+        if len(letraIncorrecta) == len(AHORCADO) - 1:
+            displayBoard(AHORCADO, letraIncorrecta, letraCorrecta, palabraSecreta)
+            print ('¡Se ha quedado sin letras!\nDespues de ' + str(len(letraIncorrecta)) + ' letras erroneas y ' + str(len(letraCorrecta)) + ' letras correctas, la palabra era "' + palabraSecreta + '"')
+            finJuego = True
+    # Pregunta al jugador si quiere jugar de nuevo
+    if finJuego:
+        if empezar():
+            letraIncorrecta = ""
+            letraCorrecta = ""
+            finJuego = False
+            palabraSecreta = buscarPalabraAleat(palabras)
+        else:
+            break
 
-
-def mostrarMano(jugador, mazo):
-    print("JUGADOR {} TURNO".format(jugador+1))
-    print("TU MAZO")
-    print("-------------------")
-    y = 1
-    for carta in mazo:
-        print("{}) {}".format(y, carta))
-        y+=1
-        print("")
-
-
-def puedoJugar(color, valor, mazo):
-    for carta in mazo:
-        if "COMODIN" in carta:
-            return True
-        elif color in carta or valor in carta:
-            return True
-            return False
-
-unoBaraja=armarBaraja();
-unoBaraja=shuffleDeck(unoBaraja);
-unoBaraja=shuffleDeck(unoBaraja);
-desecho = []
-
-jugadores = []
-colores = ["ROJO","VERDE","AZUL","AMARILLO"]
-numJugadores = int(input("DE CUANTOS JUGADORES? "))
-while numJugadores<2 or numJugadores>4:
-    numJugadores = int(input("NO VALIDO. PORFAVOR INGRESE UN NÚMERO ENTRE 2-4. DE CUANTOS JUGADORES? "))
-for jugador in range(numJugadores):
-    jugadores.append(sacarCarta(5))
-
-turnoJugador = 0
-direccionJuego = 1
-jugando = True
-desecho.append(unoBaraja.pop(0))
-separarCarta = desecho[0].split(" ", 1)
-colorActual = separarCarta[0]
-if colorActual != "COMODIN":
-    cartaVal = separarCarta[1]
-else:
-    cartaVal = "ANY1"
-
-while jugando:
-    mostrarMano(turnoJugador, jugadores[turnoJugador])
-    print("CARTAS EN LA PARTE SUPERIOR DE LA PILA DE DESECHOS: {}".format(desecho[-1]))
-    if puedoJugar(colorActual, cartaVal, jugadores[turnoJugador]):
-        cartaElegida = int(input("CON CUAL CARTA QUIERES JUGAR? "))
-while not puedoJugar(colorActual, cartaVal,[jugadores[turnoJugador][cartaElegida-1]]):
-    cartaElegida = int(input("CARTA NO VALIDA.CON CUAL CARTA QUIERES JUGAR? "))
-    print("JUGASTE {}".format(jugadores[turnoJugador][cartaElegida-1]))
-    desecho.append(jugadores[turnoJugador].pop(cartaElegida-1))
-
-if len(jugadores[turnoJugador])==0:
-    jugando = False
-    ganador = "JUGADOR {}".format(turnoJugador+1)
-else:
-    separarCarta = desecho[-1].split(" ", 1)
-    colorActual = separarCarta[0]
-
-if len(separarCarta) == 1:
-    cartaVal = "ANY"
-else:
-    cartaVal = separarCarta[1]
-if colorActual == "COMODIN":
-    for x in range(len(colores)):
-        print("{}) {}".format(x+1, colores[x]))
-    nuevoColor = "A QUE COLOR LO QUIERES CAMBIAR? "
-while nuevoColor < 1 or nuevoColor > 4:
-    nuevoColor = int(input("OPCION NO VALIDA. A QUE COLOR LO QUIERES CAMBIAR? "))
-    colorActual = colores[nuevoColor-1]
-if cartaVal == "REVERSA":
-    direccionJuego = direccionJuego * -1
-elif cartaVal == "SALTADO":
-    turnoJugador += direccionJuego
-if turnoJugador >= numJugadores:
-    turnoJugador = 0
-elif turnoJugador < 0:
-    turnoJugador = numJugadores-1
-elif cartaVal == "+2":
-    jugadora = turnoJugador+direccionJuego
-if jugadora == numJugadores:
-    jugadora = 0
-elif jugadora < 0:
-    jugadora = numJugadores-1
-    jugadores[jugadora].extend(sacarCarta(2))
-elif cartaVal == "COMODIN +4":
-    jugadora = turnoJugador+direccionJuego
-if jugadora == numJugadores:
-    jugadora = 0
-elif jugadora < 0:
-    jugadora = numJugadores-1
-    jugadores[jugadora].extend(sacarCarta(4))
-    print("")
-else:
-    print("NO PUEDES JUGAR. TIENES QUE TOMAR UNA CARTA.")
-    jugadores[turnoJugador].extend(sacarCarta(1))
-
-turnoJugador += direccionJuego
-if turnoJugador >= numJugadores:
-    turnoJugador = 0
-elif turnoJugador < 0:
-    turnoJugador = numJugadores-1
-
-print("PERDISTE ERES UN LOSER")
-print("{} ES EL GANADOR!!".format(ganador))
