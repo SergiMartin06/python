a3d4385e45b6f56f7d3aad08682d2b1ab7d32b89
Panxurri <agarcia20211@iesjoanramis.org>
2023-06-12 09:51:36 +0200

    Cambio local

--- Apuntes/api_pokemon.py
+++ Apuntes/api_pokemon.py
@@ -1,11 +1,11 @@
 import requests
-for i in range(10):
-	a = "https://pokeapi.co/api/v2/evolution-chain/"+str((i+1))
+for i in range(200):
+	a = "https://superheroapi.com/api/access-token/character-id/biography"+str((i+1))
 	res=requests.get(a)
 	if res.status_code == 200:
-    	dades = res.json()
-    	print("El nom del pokémon és: {}".format(dades["chain"]["species"]["name"]))
-    	for e in dades["chain"]["evolves_to"]:
-        	print("Nom de la seva evolució: ", e["species"]["name"])
+		dades = res.json()
+		print("El nom del pokémon és: {}".format(dades["chain"]["species"]["name"]))
+		for e in dades["chain"]["evolves_to"]:
+			print("Nom de la seva evolució: ", e["species"]["name"])
 else:
 	print("No hi ha dades.")
--- Projecto_Python/scrapping.py
+++ Projecto_Python/scrapping.py
@@ -1,20 +1,34 @@
 import requests
-from bs4 import BeautifulSoup
 
-url = 'https://es.wikipedia.org/wiki/Inteligencia_artificial'
+# Definir el nombre del superhéroe
+nombre_superheroe = "spider-man"
 
+# Construir la URL de la API con el nombre del superhéroe
+url = f"https://superheroapi.com/api/your-api-key/search/{nombre_superheroe}"
 
+# Realizar la solicitud GET a la API
 response = requests.get(url)
 
+# Verificar el estado de la respuesta
 if response.status_code == 200:
-    soup = BeautifulSoup(response.content, 'html.parser')
-    title = soup.find('h1', class_='firstHeading').text
-    print('Título:', title)
-    first_paragraph = soup.find('p').text
-    print('Primer párrafo:', first_paragraph)
-    links = soup.find_all('a')
-    print('Enlaces encontrados:')
-    for link in links:
-        print(link['href'])
+    # Obtener el resultado de la respuesta en formato JSON
+    data = response.json()
+
+    # Verificar si se encontró el superhéroe
+    if data['response'] == 'success':
+        # Obtener información sobre el primer resultado del superhéroe
+        superhero = data['results'][0]
+        
+        # Obtener el nombre, la biografía y las habilidades del superhéroe
+        nombre = superhero['name']
+        biografia = superhero['biography']
+        habilidades = superhero['powerstats']
+        
+        # Imprimir la información del superhéroe
+        print("Nombre:", nombre)
+        print("Biografía:", biografia)
+        print("Habilidades:", habilidades)
+    else:
+        print("Superhéroe no encontrado.")
 else:
-    print('Error al acceder a la página:', response.status_code)
+    print("Error al acceder a la API:", response.status_code)
